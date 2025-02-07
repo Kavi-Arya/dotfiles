51b172f25f0ba39bda9afaf9dfeb9562ce40add8
Kavi Arya <kaviarya047@tutamail.com>
2025-02-07 14:13:16 +0530

    setting nvim theme based on if dwm or hyprland is running

--- .config/NvChad/lua/chadrc.lua
+++ .config/NvChad/lua/chadrc.lua
@@ -1,8 +1,21 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.base46 = {
-	theme = "chadwal",
-}
+function M.check_environment()
+  local xorg_running = io.popen("pgrep -x dwm"):read("*a")
+  local wayland_running = io.popen("pgrep -x Hyprland"):read("*a")
 
+  if xorg_running ~= "" then
+    -- print("Detected Xorg environment.")
+    M.base46 = { theme = "vscode_dark" }
+  elseif wayland_running ~= "" then
+    -- print("Detected Wayland environment.")
+    M.base46 = { theme = "chadwal" }
+  else
+    print("No supported environment detected.")
+    M.base46 = { theme = "gruvchad" } -- Fallback theme
+  end
+end
+
+M.check_environment()
 return M
